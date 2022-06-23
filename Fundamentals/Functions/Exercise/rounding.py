287bc431df2462a9ed00417e33f529e2c36ab662
Petar Bozhkov <pbozhkov3@gmail.com>
2022-06-23 20:38:23 +0300

    Add files via upload

---
+++ Fundamentals/Functions/Exercise/rounding.py
@@ -0,0 +1,9 @@
+print(list(map(lambda x: round(float(x)), input().split(' '))))
+
+# my solution
+# numbers = input().split(' ')
+# round_numbers = []
+#
+# for num in numbers:
+#     round_numbers.append(round(float(num)))
+# print(round_numbers)
