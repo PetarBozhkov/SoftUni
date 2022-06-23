dae7fd86ebf8d35331f08584b5fba6204bea6373
Petar Bozhkov <pbozhkov3@gmail.com>
2022-06-23 20:37:27 +0300

    Add files via upload

---
+++ Fundamentals/Functions/Exercise/calculate_rectangle_area.py
@@ -0,0 +1,13 @@
+def area_of_a_rectangle(width, height):
+    return width * height
+
+
+side_one = int(input())
+side_two = int(input())
+print(area_of_a_rectangle(side_one, side_two))
+
+# my solution
+# width = int(input())
+# height = int(input())
+# area = width * height
+# print(area)
