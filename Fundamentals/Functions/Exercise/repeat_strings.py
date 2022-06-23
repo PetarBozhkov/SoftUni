861529a05f525180be73fb8884b726121b226de7
Petar Bozhkov <pbozhkov3@gmail.com>
2022-06-23 20:37:51 +0300

    Add files via upload

---
+++ Fundamentals/Functions/Exercise/repeat_strings.py
@@ -0,0 +1,15 @@
+# result = lambda a, b: a * b
+
+def repeat_string(a, b):
+    return a * b
+
+
+string = input()
+number = int(input())
+print(repeat_string(string, number))
+
+# my solution
+# text = input()
+# counter = int(input())
+# result = text * counter
+# print(f"{result}")
