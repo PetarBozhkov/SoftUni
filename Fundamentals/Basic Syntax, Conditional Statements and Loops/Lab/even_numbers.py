431c8f5e289c09c1c7c24224569655a2cd3721b9
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-03 21:30:03 +0300

    Update even_numbers.py

--- Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/even_numbers.py
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/even_numbers.py
@@ -1,3 +1,10 @@
+# 4. Even Numbers
+
+# Write a program that receives a number n on the first line. 
+# On the following n lines, it receives different integer numbers. 
+# If the program receives an odd number, print "{num} is odd!" and end the program. 
+# Otherwise, if all numbers given are even, print "All numbers are even.".
+
 number_of_lines = int(input())
 
 for _ in range(number_of_lines):
