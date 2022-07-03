d4b4186c400004a95c4d918f0e7ac63de7c411d0
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-03 21:19:27 +0300

    Update number_definer.py

--- Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/number_definer.py
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/number_definer.py
@@ -1,4 +1,14 @@
-number = float(input())
+# 1. Number Definer
+
+# Write a program that reads a floating-point number and:
+
+# - prints "zero" if the number is zero
+
+# - prints "positive" or "negative"
+
+# - adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds 1 000 000
+
+# number = float(input())
 
 if number == 0:
     print("zero")
