ad9c4a34f5bf95342b4f5bf500e0ca916d8587ad
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-25 21:30:14 +0300

    Update integer_operations.py

--- Fundamentals/Data Types and Variables/Exercise/integer_operations.py
+++ Fundamentals/Data Types and Variables/Exercise/integer_operations.py
@@ -1,3 +1,9 @@
+#1. Integer Operations
+
+#Write a program that reads four integer numbers. 
+#It should add the first to the second number, integer divide the sum by the third number, and multiply the result by the fourth number.
+#Print the final result.
+
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
