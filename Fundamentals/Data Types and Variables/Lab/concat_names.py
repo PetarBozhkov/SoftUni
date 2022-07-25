7f71200984cd779de1140e45666242cb2504f1cc
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-25 21:24:30 +0300

    Update concat_names.py

--- Fundamentals/Data Types and Variables/Lab/concat_names.py
+++ Fundamentals/Data Types and Variables/Lab/concat_names.py
@@ -1,3 +1,7 @@
+#1. Concat Names
+
+#Write a program that reads two names and a delimiter. It should print the names joined by the delimiter.
+
 name_one = input()
 name_two = input()
 limiter = input()
