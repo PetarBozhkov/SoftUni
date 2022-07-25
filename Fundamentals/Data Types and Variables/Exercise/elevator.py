9889817b5fe6e9166e97c46f9fd2b21ed2d2a9e0
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-25 21:32:36 +0300

    Update elevator.py

--- Fundamentals/Data Types and Variables/Exercise/elevator.py
+++ Fundamentals/Data Types and Variables/Exercise/elevator.py
@@ -1,3 +1,8 @@
+#3. Elevator
+
+#Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons. 
+#The input holds two lines: the number of people N and the capacity P of the elevator.
+
 import math
 number_of_people = int(input())
 capacity = int(input())
