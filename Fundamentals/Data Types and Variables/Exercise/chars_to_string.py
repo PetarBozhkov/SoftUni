df32bc943b5c01b51d95c79819f33946e0b8f741
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-25 21:30:54 +0300

    Update chars_to_string.py

--- Fundamentals/Data Types and Variables/Exercise/chars_to_string.py
+++ Fundamentals/Data Types and Variables/Exercise/chars_to_string.py
@@ -1,3 +1,7 @@
+#2. Chars to String
+
+#Write a function that receives 3 characters. Concatenate all the characters into one string and print it on the console.
+
 first_char = input()
 second_char = input()
 third_char = input()
