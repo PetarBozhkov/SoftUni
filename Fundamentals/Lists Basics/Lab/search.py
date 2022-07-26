6a51609272c9d872ca137d67ff13901be6697a2d
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-26 16:50:32 +0300

    Update search.py

--- Fundamentals/Lists Basics/Lab/search.py
+++ Fundamentals/Lists Basics/Lab/search.py
@@ -1,3 +1,9 @@
+#4. Search
+
+#On the first line, you will receive a number n. On the second line, you will receive a word. On the following n lines, you will be given some strings. 
+#You should add them to a list and print them. 
+#After that, you should filter out only the strings that include the given word and print that list too.
+
 number = int(input())
 word = input()
 list_one = []
