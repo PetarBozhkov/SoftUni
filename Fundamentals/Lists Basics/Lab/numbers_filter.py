a46831d633648e1a990830d6d986d073136a1157
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-26 16:52:58 +0300

    Update numbers_filter.py

--- Fundamentals/Lists Basics/Lab/numbers_filter.py
+++ Fundamentals/Lists Basics/Lab/numbers_filter.py
@@ -1,3 +1,17 @@
+#5. Numbers Filter
+
+#On the first line, you will receive a single number n. On the following n lines, you will receive integers. 
+#After that, you will be given one of the following commands:
+
+#· even
+
+#· odd
+
+#· negative
+
+#· positive
+#Filter all the numbers that fit in the category (0 counts as a positive and even). Finally, print the result.
+
 n = int(input())
 
 command_even = 'even'
