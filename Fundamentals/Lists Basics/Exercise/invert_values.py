2adea0d2444ebbe69eb8b9bc34646420c861a540
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-27 18:46:41 +0300

    Update invert_values.py

--- Fundamentals/Lists Basics/Exercise/invert_values.py
+++ Fundamentals/Lists Basics/Exercise/invert_values.py
@@ -1,3 +1,8 @@
+#1. Invert Values
+
+#Write a program that receives a single string containing positive and negative numbers separated by a single space. 
+#Print a list containing the opposite of each number.
+
 list_of_numbers = input().split()
 opposite_numbers = []
 
