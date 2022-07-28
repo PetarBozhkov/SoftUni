194274c9237455e6be1295c8510a14702280b141
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-28 16:48:11 +0300

    Update rounding.py

--- Fundamentals/Functions/Exercise/rounding.py
+++ Fundamentals/Functions/Exercise/rounding.py
@@ -1,3 +1,7 @@
+#7. Rounding
+
+#Write a program that rounds all the given numbers, separated by a single space, and prints the result as a list. Use round().
+
 print(list(map(lambda x: round(float(x)), input().split(' '))))
 
 # my solution
