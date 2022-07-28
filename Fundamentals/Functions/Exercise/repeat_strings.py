afba07e0f68022a7890db58ebd29d426ee658e36
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-28 16:41:04 +0300

    Update repeat_strings.py

--- Fundamentals/Functions/Exercise/repeat_strings.py
+++ Fundamentals/Functions/Exercise/repeat_strings.py
@@ -1,3 +1,9 @@
+#4. Repeat String
+
+#Write a function that receives a string and a counter n. 
+#The function should return a new string – the result of repeating the old string n times. 
+#Print the result of the function. Try using lambda.
+
 # result = lambda a, b: a * b
 
 def repeat_string(a, b):
