206db0bedf0860cb2f8a8bffe2bee3102aa1b748
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-28 16:40:18 +0300

    Update calculations.py

--- Fundamentals/Functions/Exercise/calculations.py
+++ Fundamentals/Functions/Exercise/calculations.py
@@ -1,3 +1,10 @@
+#3. Calculations
+
+#Create a function that receives three parameters, calculates a result depending on the given operator, and returns it. Print the result of the function.
+
+#The input comes as three parameters – an operator as a string and two integer numbers. 
+#The operator can be one of the following: "multiply", "divide", "add", "subtract".
+
 def calculation_func(number_a, number_b, operation):
     if operation == 'multiply':
         return number_a * number_b
