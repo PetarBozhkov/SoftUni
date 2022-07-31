774bd20dc5a81a31e4d17656f3cb6b96e5874c0d
Petar Bozhkov <pbozhkov3@gmail.com>
2022-07-31 14:18:29 +0300

    Update counter_strike.py

--- Fundamentals/counter_strike.py
+++ Fundamentals/counter_strike.py
@@ -1,3 +1,19 @@
+#1. Counter-Strike Submit your solutions in the SoftUni judge system at https://judge.softuni.org/Contests/Practice/Index/2305#0.
+
+#Write a program that keeps track of every won battle against an enemy. You will receive initial energy. 
+#Afterward, you will start receiving the distance you need to reach an enemy until the "End of battle" command is given or you run out of energy.
+
+#The energy you need to reach an enemy is equal to the distance you receive. 
+#Each time you reach an enemy, you win a battle, and your energy is reduced. 
+#Otherwise, if you don't have enough energy to reach an enemy, end the program and print: 
+#"Not enough energy! Game ends with {count} won battles and {energy} energy".
+
+#Every third won battle increases your energy with the value of your current count of won battles.
+
+#Upon receiving the "End of battle" command, print the count of won battles in the following format:
+
+#"Won battles: {count}. Energy left: {energy}"
+
 initial_energy = int(input())
 won_battle_counter = 0
 
