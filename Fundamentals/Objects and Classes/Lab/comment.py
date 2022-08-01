fe303b2ebbe3409bb89eefd1508ac99ada8a12ee
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-01 17:35:03 +0300

    Update comment.py

--- Fundamentals/Objects and Classes/Lab/comment.py
+++ Fundamentals/Objects and Classes/Lab/comment.py
@@ -1,15 +1,9 @@
 #1. Comment
-
 #Create a class with the name "Comment". The __init__ method should accept 3 parameters:
-
 #· username
-
 #· content
-
 #· likes (optional, 0 by default)
-
 #Use the exact names for your variables
-
 #Note: there is no input/output for this problem. Test the class yourself and submit only the class
 
 class Comment:
