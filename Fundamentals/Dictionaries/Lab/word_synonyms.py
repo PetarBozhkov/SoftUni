8b0728bbb446a5b2a05f615b0b0ed63c1dea3edd
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-22 18:30:29 +0300

    Add files via upload

---
+++ Fundamentals/Dictionaries/Lab/word_synonyms.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+synonyms = defaultdict(list)
+number = int(input())
+
+for _ in range(number):
+    word, synonym = input(), input()
+    synonyms[word].append(synonym)
+
+data = [f"{word} - {', '.join(synonym)}" for word, synonym in synonyms.items()]
+print('\n'.join(data))
+
+#n = int(input())
+#synonyms = {}
+
+#for i in range(n):
+#    word = input()
+#    synonym = input()
+#    if word not in synonyms:
+#        synonyms[word] = []
+#    synonyms[word].append(synonym)
+
+#for word in synonyms:
+#    print(f"{word} - {', '.join(synonyms[word])}")
