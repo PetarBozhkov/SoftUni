ec85ff41d3ec2e09aad6a536531a88a85cebe307
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-23 18:01:49 +0300

    Update ascii_values.py

--- Fundamentals/Dictionaries/Lab/ascii_values.py
+++ Fundamentals/Dictionaries/Lab/ascii_values.py
@@ -1,3 +1,9 @@
+#5. ASCII Values
+
+#Write a program that receives a list of characters separated by ", ". 
+#It should create a dictionary with each character as a key and its ASCII value as a value. 
+#Try solving that problem using comprehension.
+
 data = input().split(", ")
 char_values = {key: ord(key) for key in data}
 print(char_values)
