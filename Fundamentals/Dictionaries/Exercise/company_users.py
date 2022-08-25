6dfca917e67aa29f5fca2f4a0e37542c1052eadb
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-25 15:17:47 +0300

    Update company_users.py

--- Fundamentals/Dictionaries/Exercise/company_users.py
+++ Fundamentals/Dictionaries/Exercise/company_users.py
@@ -1,3 +1,22 @@
+#10. Company Users
+
+#Write a program that keeps the information about companies and their employees.
+
+#You will be receiving company names and an employees' id until you receive the command "End" command. Add each employee to the given company. 
+#Keep in mind that a company cannot have two employees with the same id.
+
+#Print the company name and each employee's id in the following format:
+
+#"{company_name}
+
+#-- {id1}
+
+#-- {id2}
+
+#…
+
+#-- {idN}"
+
 force_side_dict = {}
 command = input()
 
