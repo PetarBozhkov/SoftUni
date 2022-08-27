7fffdebe965329447b6fadb8e96ab6bd049c8e5b
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-27 17:31:35 +0300

    Update reverse_strings.py

--- Fundamentals/Text Processing/Lab/reverse_strings.py
+++ Fundamentals/Text Processing/Lab/reverse_strings.py
@@ -1,3 +1,8 @@
+#1. Reverse Strings
+
+#You will be given strings on separate lines until you receive an "end" command. 
+#Write a program that reverses strings and prints each pair on a separate line in the format "{word} = {reversed_word}".
+
 text = input()
 while text != 'end':
     reversed_text = text[::-1]
