4cf1e8d671a21f74992ce3ace9d03c47fc228340
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-27 17:35:38 +0300

    Update substring.py

--- Fundamentals/Text Processing/Lab/substring.py
+++ Fundamentals/Text Processing/Lab/substring.py
@@ -1,7 +1,8 @@
 #3. Substring
 
 #On the first line, you will receive a string. On the second line, you will receive a second string. 
-#Write a program that removes all the occurrences of the first string in the second until there is no match. At the end, print the remaining string.
+#Write a program that removes all the occurrences of the first string in the second until there is no match. 
+#At the end, print the remaining string.
 
 removing_word = input()
 the_word = input()
