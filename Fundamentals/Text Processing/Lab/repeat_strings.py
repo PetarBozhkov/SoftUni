4b18978201cc35781ede79d1421cf33cd332e9c1
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-27 17:32:06 +0300

    Update repeat_strings.py

--- Fundamentals/Text Processing/Lab/repeat_strings.py
+++ Fundamentals/Text Processing/Lab/repeat_strings.py
@@ -1,3 +1,9 @@
+#2. Repeat Strings
+
+#Write a program that reads a sequence of strings, separated by a single space. 
+#Each string should be repeated N times, where N is the length of the string. 
+#Print the final strings concatenated into one string.
+
 texts = input().split(" ")
 output = ""
 
