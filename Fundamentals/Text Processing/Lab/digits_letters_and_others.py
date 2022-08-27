3679413b640857ef76a1c8509ba71f50d83fa30b
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-27 17:34:14 +0300

    Update digits_letters_and_others.py

--- Fundamentals/Text Processing/Lab/digits_letters_and_others.py
+++ Fundamentals/Text Processing/Lab/digits_letters_and_others.py
@@ -1,3 +1,8 @@
+#5. Digits, Letters, and Other
+
+#Write a program that receives a single string. On the first line, print all the digits found in the string, on the second – all the letters, and on the third – all the other characters. 
+#There will always be at least one digit, one letter, and one other character.
+
 text = list(input())
 digits = ""
 letters = ""
