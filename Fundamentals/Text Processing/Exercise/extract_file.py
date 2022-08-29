e735b8a709568e060c7572f747ac760ae221c474
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-29 19:22:10 +0300

    Update extract_file.py

--- Fundamentals/Text Processing/Exercise/extract_file.py
+++ Fundamentals/Text Processing/Exercise/extract_file.py
@@ -1,3 +1,7 @@
+#3. Extract File
+
+#Write a program that reads the path to a file and subtracts the file name and its extension.
+
 file_path = input().split("\\")
 file_name = file_path[-1].split(".")[0]
 file_extension = file_path[-1].split(".")[-1]
