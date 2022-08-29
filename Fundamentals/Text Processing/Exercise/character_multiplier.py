c6da34c90436c2775d5eb02b2b5f4fa3a7ec72d8
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-29 19:21:38 +0300

    Update character_multiplier.py

--- Fundamentals/Text Processing/Exercise/character_multiplier.py
+++ Fundamentals/Text Processing/Exercise/character_multiplier.py
@@ -1,3 +1,10 @@
+#2. Character Multiplier
+
+#Create a program that receives two strings on a single line separated by a single space. 
+#Then, it prints the sum of their multiplied character codes as follows: multiply str1[0] with str2[0] and add the result to the total sum, 
+#then continue with the next two characters. 
+#If one of the strings is longer than the other, add the remaining character codes to the total sum without multiplication.
+
 def sum_of_char_code(first, second):
     total_sum = 0
     for i in range(len(first)):
