0d38fdce61df76732ab831f1f9991ba6cdfa576f
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-29 19:29:03 +0300

    Update letters_change_numbers.py

--- Fundamentals/Text Processing/Exercise/letters_change_numbers.py
+++ Fundamentals/Text Processing/Exercise/letters_change_numbers.py
@@ -1,3 +1,20 @@
+#8. *Letters Change Numbers
+
+#John invented a game with numbers and letters from the English alphabet. The game was simple.
+
+#You receive a string consisting of a number between two letters. For the given string, you should perform different mathematical operations to achieve a result:
+
+#· First, if the letter in front of the number is:
+
+#o Uppercase: divide the number by the letter's position in the alphabet (starting from 1)
+#o Lowercase: multiply the number with the letter's position in the alphabet (starting from 1)
+#· Next, if the letter after the number is:
+#o Uppercase: subtract its position from the resulting number (starting from 1)
+#o Lowercase: add its position to the resulting number (starting from 1)
+
+#The game was too easy for John. He decided to complicate it by doing the same calculations to multiple strings keeping track of only the total sum of all results. Once he started to solve this with more strings and bigger numbers, it became quite hard to do it only in his mind.
+#He kindly asks you to write a program that performs the operations described above and sums the final results of each string.
+
 from string import ascii_lowercase
 
 words = input().split()
