7f8f75d8c75728da33e12e027807b0514f0aaf4f
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-29 19:20:44 +0300

    Update valid_usernames.py

--- Fundamentals/Text Processing/Exercise/valid_usernames.py
+++ Fundamentals/Text Processing/Exercise/valid_usernames.py
@@ -1,3 +1,15 @@
+#1. Valid Usernames
+
+#Write a program that reads usernames on a single line (separated by ", ") and prints all valid usernames on separate lines.
+
+#A valid username:
+
+#· has length between 3 and 16 characters inclusive
+
+#· can contain only letters, numbers, hyphens, and underscores
+
+#· has no redundant symbols before, after, or in between
+
 data = input().split(", ")
 usernames = list()
 for user in data:
