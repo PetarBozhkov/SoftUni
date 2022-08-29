b5b2582681a44f32bdb59ca48f70720d5746acf6
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-29 19:23:31 +0300

    Update ceaser_cipher.py

--- Fundamentals/Text Processing/Exercise/ceaser_cipher.py
+++ Fundamentals/Text Processing/Exercise/ceaser_cipher.py
@@ -1,3 +1,9 @@
+#4. Caesar Cipher
+
+#Write a program that returns an encrypted version of the same text. 
+#Encrypt the text by replacing each character with the corresponding character three positions forward in the ASCII table. 
+#For example, A would be replaced with D, B would become E, and so on. Print the encrypted text.
+
 text = input()
 output = ""
 for ch in text:
