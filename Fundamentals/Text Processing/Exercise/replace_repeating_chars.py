96228a79899c181440eaa4f115906c29fe801659
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-29 19:27:01 +0300

    Update replace_repeating_chars.py

--- Fundamentals/Text Processing/Exercise/replace_repeating_chars.py
+++ Fundamentals/Text Processing/Exercise/replace_repeating_chars.py
@@ -1,3 +1,7 @@
+#6. Replace Repeating Chars
+
+#Write a program that reads a string from the console and replaces any sequence of the same letters with a single corresponding letter.
+
 text = input()
 output = ""
 
