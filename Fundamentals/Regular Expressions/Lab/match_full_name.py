d83a0778dbbbe0f3c98d7e15eac74da68c51b054
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-31 16:31:53 +0300

    Update match_full_name.py

--- Fundamentals/Regular Expressions/Lab/match_full_name.py
+++ Fundamentals/Regular Expressions/Lab/match_full_name.py
@@ -1,3 +1,7 @@
+#1. Match Full Name
+
+#Write a program to match full names from a sequence of characters and print them on the console.
+
 import re
 phones = input()
 expression = r"\+359([ -])2\1\d{3}\1\d{4}\b"
