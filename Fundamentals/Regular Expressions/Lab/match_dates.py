ccd0cc4ce0e4c557dfef6b3ef2907e499a56eee1
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-31 16:39:00 +0300

    Update match_dates.py

--- Fundamentals/Regular Expressions/Lab/match_dates.py
+++ Fundamentals/Regular Expressions/Lab/match_dates.py
@@ -1,3 +1,7 @@
+#3. Match Dates
+
+#Write a program, which matches a date in the format "dd{separator}MMM{separator}yyyy". Use capturing groups in your regular expression.
+
 import re
 dates = input()
 expression = r"(?P<day>\d{2})(?P<separator>[./-])(?P<month>[A-Z][a-z]{2})\2(?P<year>\d{4})"
