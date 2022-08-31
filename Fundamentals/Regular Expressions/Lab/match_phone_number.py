29e4e9af2af9bb78b8359b2191ce0d8af3ef8961
Petar Bozhkov <pbozhkov3@gmail.com>
2022-08-31 16:43:04 +0300

    Update match_phone_number.py

--- Fundamentals/Regular Expressions/Lab/match_phone_number.py
+++ Fundamentals/Regular Expressions/Lab/match_phone_number.py
@@ -1,5 +1,4 @@
 #2. Match Phone Number
-
 #Write a regular expression to match a valid phone number from Sofia. 
 #After you find all valid phones, print them on the console, separated by a comma and a space ", ".
 
