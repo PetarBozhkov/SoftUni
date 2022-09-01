452fa28564e6773f6e1e267bfa3d19d4d72371fe
Petar Bozhkov <pbozhkov3@gmail.com>
2022-09-01 18:18:12 +0300

    Update furniture.py

--- Fundamentals/Regular Expressions/Exercise/furniture.py
+++ Fundamentals/Regular Expressions/Exercise/furniture.py
@@ -4,6 +4,7 @@ product_info = input()
 total_amount = 0
 furniture_name = list()
 expression = r"\>{2}(?P<product_name>\w+)\<{2}(?P<price>\d+(\.\d+)?)\!(?P<quantity>\d+)"
+
 while product_info !="Purchase":
     matches_product = re.finditer(expression, product_info)
     for match in matches_product:
@@ -14,9 +15,9 @@ while product_info !="Purchase":
         quantity = match.group("quantity")
         furniture_name.append(product_name)
         total_amount += float(price) * int(quantity)
-
     product_info = input()
 print("Bought furniture:")
+
 for furniture in furniture_name:
     print(furniture)
 print(f"Total money spend: {total_amount:.2f}")
