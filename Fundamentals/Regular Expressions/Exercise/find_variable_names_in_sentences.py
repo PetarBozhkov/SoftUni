24071e4c14e3a556bf95718d808a8f7dcaf22fd4
Petar Bozhkov <pbozhkov3@gmail.com>
2022-09-02 14:05:48 +0300

    Update find_variable_names_in_sentences.py

--- Fundamentals/Regular Expressions/Exercise/find_variable_names_in_sentences.py
+++ Fundamentals/Regular Expressions/Exercise/find_variable_names_in_sentences.py
@@ -1,3 +1,10 @@
+#2. Find Variable Names in Sentences
+
+#Write a program that finds all variable names in each string. 
+#A variable name starts with an underscore ("_") and contains only capital and non-capital letters and digits. 
+#Extract only their names without the underscore. Try to do this only with regular expressions.
+#The output consists of all variable names extracted and printed on a single line, separated by a comma.
+
 import re
 
 text = input()
