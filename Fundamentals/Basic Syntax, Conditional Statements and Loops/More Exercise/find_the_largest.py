30822c4f3ae647ca860d376192608f9fe8d4bf03
Petar Bozhkov <pbozhkov3@gmail.com>
2022-09-04 18:25:56 +0300

    Update find_the_largest.py

--- Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/find_the_largest.py
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/find_the_largest.py
@@ -1,3 +1,7 @@
+#1. Find the Largest
+
+#You will be given a number. Print the largest number that can be formed from the digits of the given number.
+
 numbers = list(input())
 for number in range(len(numbers)):
     print(max(numbers), end='')
