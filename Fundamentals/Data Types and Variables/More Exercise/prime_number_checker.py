6f49e653a78abb6787f65517aac0189c0a7bda61
Petar Bozhkov <pbozhkov3@gmail.com>
2022-09-13 19:25:21 +0300

    Update prime_number_checker.py

--- Fundamentals/Data Types and Variables/More Exercise/prime_number_checker.py
+++ Fundamentals/Data Types and Variables/More Exercise/prime_number_checker.py
@@ -1,3 +1,11 @@
+#2. Prime Number Checker
+
+#Write a program to check if a number is prime. A prime number is a natural number greater than 1, not a product of two smaller natural numbers. For example, the only ways of writing 5 as a product, 1 × 5 or 5 × 1, involve 5 itself.
+
+#The input comes as an integer number.
+
+#The output should be True if the number is prime and False otherwise.
+
 num = int(input())
 if num>1:
     for i in range(2,num//2):
