ba72cf2639671875e23546bd6376a0311b9c2bf6
Petar Bozhkov <pbozhkov3@gmail.com>
2022-09-13 19:24:41 +0300

    Update exchange_integers.py

--- Fundamentals/Data Types and Variables/More Exercise/exchange_integers.py
+++ Fundamentals/Data Types and Variables/More Exercise/exchange_integers.py
@@ -1,3 +1,7 @@
+#1. Exchange Integers
+
+#Read two integer numbers and, after that, exchange their values. Print the variable values before and after the exchange, as shown below:
+
 first_number = int(input())
 second_number = int(input())
 
