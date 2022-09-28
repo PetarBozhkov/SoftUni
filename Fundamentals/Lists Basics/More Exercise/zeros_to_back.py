eb20975980094ead822866d52a23dabc58d321c3
Petar Bozhkov <pbozhkov3@gmail.com>
2022-09-28 17:20:33 +0300

    Update zeros_to_back.py

--- Fundamentals/Lists Basics/More Exercise/zeros_to_back.py
+++ Fundamentals/Lists Basics/More Exercise/zeros_to_back.py
@@ -1,3 +1,7 @@
+#1. Zeros to Back
+#Write a program that receives a single string (integers separated by a comma and space ", "), finds all the zeros, and moves them to the back without messing up the other elements. 
+#Print the resulting integer list.
+
 the_string = input().replace(', ',' ').split()
 the_list = []
 count = 0
