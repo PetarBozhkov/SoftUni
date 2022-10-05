37f563754508da02608f84baeff204baeeb46be1
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-05 23:36:10 +0300

    Update data_types.py

--- Fundamentals/Functions/More Exercise/data_types.py
+++ Fundamentals/Functions/More Exercise/data_types.py
@@ -1,3 +1,10 @@
+#1. Data Types
+#Write a function that, depending on the first line of the input, reads one of the following strings: "int", "real", or "string".
+#· If the data type is an int, multiply the number by 2.
+#· If the data type is real, multiply the number by 1.5 and format the result to the second decimal point.
+#· If the data type is a string, surround the input with "$".
+#Print the result on the console.
+
 def data_type(type: str, parametur):
     if type == 'int':
         return int(parametur) * 2
