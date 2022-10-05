55279a924f4a518c30922de0f5631b6dcd67649a
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-06 00:04:43 +0300

    Update tirbonacci_sequence.py

--- Fundamentals/Functions/More Exercise/tirbonacci_sequence.py
+++ Fundamentals/Functions/More Exercise/tirbonacci_sequence.py
@@ -1,5 +1,4 @@
 #4. Tribonacci Sequence
-
 #In the Tribonacci sequence, every number is formed by the sum of the previous 3.
 #Write a function that prints numbers from the Tribonacci sequence on one line separated by a single space, starting from 1. 
 #You will receive a positive integer number as input.
