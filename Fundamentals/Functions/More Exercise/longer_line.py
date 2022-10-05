0601d3c4f5333a5dad1455fb35e8e0054373d5da
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-06 00:03:55 +0300

    Update longer_line.py

--- Fundamentals/Functions/More Exercise/longer_line.py
+++ Fundamentals/Functions/More Exercise/longer_line.py
@@ -1,4 +1,5 @@
 #3. Longer Line
+
 #You will be given the coordinates of four points. The first and the second pair of points form two different lines. 
 #Create a function that prints the longer line in the format "({X1}, {Y1})({X2}, {Y2})" starting from the point which is closer to the center of the coordinate system (0, 0). 
 #You can reuse the method that you wrote for the previous problem. 
