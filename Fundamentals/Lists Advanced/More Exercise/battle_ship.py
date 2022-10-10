e64b4cc681aab85342270aee1cc6dfa931603e42
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-10 23:37:03 +0300

    Update battle_ship.py

--- Fundamentals/Lists Advanced/More Exercise/battle_ship.py
+++ Fundamentals/Lists Advanced/More Exercise/battle_ship.py
@@ -1,9 +1,11 @@
 rows_of_the_field = int(input())
 battle_ships = list()
+
 for row in range(rows_of_the_field):
     battle_ships.append(list(map(int, input().split(' '))))
 attacks = input().split(' ')
 destroyed_ship = 0
+
 for attack in range(len(attacks)):
     attacked_row = int(attacks[attack][0])
     attacked_col = int(attacks[attack][2])
