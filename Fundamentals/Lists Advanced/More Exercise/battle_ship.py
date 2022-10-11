15e7c475e040222968a56d5bd51e73548410f595
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-11 22:02:04 +0300

    Update battle_ship.py

--- Fundamentals/Lists Advanced/More Exercise/battle_ship.py
+++ Fundamentals/Lists Advanced/More Exercise/battle_ship.py
@@ -1,4 +1,5 @@
 #4. Battle Ships
+
 #You will be given a number n representing the number of rows of the field. 
 #On the following n lines, you will receive each field row as a string with numbers separated by a space. 
 #Each number greater than zero represents a ship with health equal to the number value.
