46240d077b0e9582113bfc2bdb4706a5ea0adfc4
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-11 22:01:14 +0300

    Update battle_ship.py

--- Fundamentals/Lists Advanced/More Exercise/battle_ship.py
+++ Fundamentals/Lists Advanced/More Exercise/battle_ship.py
@@ -1,3 +1,12 @@
+#4. Battle Ships
+
+#You will be given a number n representing the number of rows of the field. 
+#On the following n lines, you will receive each field row as a string with numbers separated by a space. 
+#Each number greater than zero represents a ship with health equal to the number value.
+#After that, you will receive the squares that are being attacked in the format: "{row}-{col} {row}-{col}". 
+#Each time a square is being attacked, if there is a ship (number greater than 0), you should reduce its value by 1. 
+#If a ship's health reaches zero, it is destroyed. After the attacks have ended, print how many ships were destroyed.
+
 rows_of_the_field = int(input())
 battle_ships = list()
 
