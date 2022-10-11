36fe89a2517c0bdef019e7bf100a12675e8cf4de
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-11 21:57:14 +0300

    Update social_distribution.py

--- Fundamentals/Lists Advanced/More Exercise/social_distribution.py
+++ Fundamentals/Lists Advanced/More Exercise/social_distribution.py
@@ -1,3 +1,11 @@
+#1. Social Distribution
+#A core idea of several left-wing ideologies is that the wealthiest should support the poorest, no matter what, and that is exactly what you are called to do for this problem.
+#On the first line, you will be given the population (numbers separated by comma and space ", "). 
+#On the second line, you will be given the minimum wealth. 
+#You should distribute the wealth so that no part of the population has less than the minimum wealth. 
+#To do that, you should always take wealth from the wealthiest part of the population.
+#There will be cases where the distribution will not be possible. In that case, print: "No equal distribution possible".
+
 def poor_rich(possessions: list, wealth: int):
     for index in range(len(possessions)):
         if possessions[index] < wealth:
