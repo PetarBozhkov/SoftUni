26bf46420af245bf6992e8a0626e0c3a6a1a1e0e
Petar Bozhkov <pbozhkov3@gmail.com>
2022-10-11 22:00:26 +0300

    Update kates_way_out.py

--- Fundamentals/Lists Advanced/More Exercise/kates_way_out.py
+++ Fundamentals/Lists Advanced/More Exercise/kates_way_out.py
@@ -1,3 +1,15 @@
+#3. Kate's Way Out
+#Kate is stuck in a maze. You should help her to find her way out.
+#On the first line, you will be given how many rows there are in the maze. 
+#On the following n lines, you will be given the maze itself. Here is a legend for the maze:
+#· "#" - means a wall; Kate cannot go through there
+#· " " - means empty space; Kate can go through there
+#· "k" - the initial position of Kate; start looking for a way out from there
+#There are two options: Kate either gets out or not:
+#· If Kate can get out, print the following: "Kate got out in {number_of_moves} moves".
+#Note: If there are two or more ways out, she always chooses the longest one.
+#· Otherwise, print: "Kate cannot get out".
+
 def remove_all_hashtag(matrix: list):
     for row in matrix:
         row = [i for i in row if i != '#']
