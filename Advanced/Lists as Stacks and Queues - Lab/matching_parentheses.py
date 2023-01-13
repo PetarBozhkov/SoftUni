f205d14720110ef86553e567b913d586202d9c00
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-13 10:39:33 +0200

    Update matching_parentheses.py

--- Advanced/Lists as Stacks and Queues - Lab/matching_parentheses.py
+++ Advanced/Lists as Stacks and Queues - Lab/matching_parentheses.py
@@ -1,7 +1,5 @@
 #2. Matching Parentheses
-
 #You are given an algebraic expression with parentheses. Scan through the string and extract each set of parentheses.
-
 #Print the result back on the console.
 
 data = input()
