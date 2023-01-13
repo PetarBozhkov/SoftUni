b3a47a26f99eccdfb6c8ef4f3a24c868e51564ec
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-13 10:34:22 +0200

    Update reverse_strings.py

--- Advanced/Lists as Stacks and Queues - Lab/reverse_strings.py
+++ Advanced/Lists as Stacks and Queues - Lab/reverse_strings.py
@@ -1,3 +1,13 @@
+#1. Reverse Strings
+
+#Write program that:
+
+#· Reads an input string
+
+#· Reverses it using a stack
+
+#· Prints the result back on the console
+
 stack = list(input())
 
 while len(stack) > 0:
