ed768f565e47c622acb1bc3fc033e6e19b8984f3
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-15 00:51:31 +0200

    Update cups_and_bottles.py

--- Advanced/Lists as Stacks and Queues - Exercise/cups_and_bottles.py
+++ Advanced/Lists as Stacks and Queues - Exercise/cups_and_bottles.py
@@ -1,3 +1,17 @@
+#10. *Cups and Bottles
+#You will be given a sequence of integers – each indicating a cup's capacity (in litters). 
+#After that, you will be given another sequence of integers – each indicating a bottle's capacity (in litters). 
+#Your job is to try to fill up all the cups.
+#You must start picking from the last received bottle and start filling from the first entered cup. 
+#You could pick exactly one bottle at a time. If the current bottle has N water, you give the first entered cup N water and reduce its integer value by N.
+#When a cup's integer value reaches 0 or less, it gets removed. 
+#It is possible that the current cup's value is greater than the current bottle's value. 
+#In that case, you pick bottles until you reduce the cup's integer value to 0 or less. 
+#If a bottle's value is greater or equal to the cup's current value, you fill up the cup, and the remaining water becomes wasted. 
+#You should keep track of the wasted litters of water and print them at the end of the program.
+#If you have managed to fill up all the cups, print the remaining water bottles, from the last entered – to the first. 
+#Otherwise, you must print the remaining cups ordered by the entrance – from the first entered – to the last.
+
 from collections import deque
 
 cups = deque([int(cup) for cup in input().split()])
