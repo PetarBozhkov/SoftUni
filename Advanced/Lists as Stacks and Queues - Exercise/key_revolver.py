35846d295891ddc7c8b360adbef0dd9cc131b193
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-15 00:49:54 +0200

    Update key_revolver.py

--- Advanced/Lists as Stacks and Queues - Exercise/key_revolver.py
+++ Advanced/Lists as Stacks and Queues - Exercise/key_revolver.py
@@ -1,3 +1,15 @@
+#9. *Key Revolver
+#Our favorite super-spy action hero Sam is back from his vacation, and it is time to go on a mission. 
+#He needs to unlock a safe locked by several locks in a row, which all have varying sizes.
+#The hero possesses a special weapon called the Key Revolver, with special bullets. 
+#Each bullet can unlock a lock with a size equal to or larger than the size of the bullet. 
+#The bullet goes into the keyhole, then explodes, completely destroying it. 
+#Sam doesn't know the size of the locks, so he needs to just shoot at all of them until the safe runs out of locks.
+#What's behind the safe, you ask? Well, intelligence! It is told that Sam's sworn enemy – Nikoladze, keeps his top-secret Georgian Chacha Brandy recipe inside. 
+#It's valued differently across different times of the year, so Sam's boss will tell him what it's worth over the radio. 
+#One last thing, every bullet Sam fires will also cost him money, which will be deducted from his pay from the price of the intelligence.
+#Good luck, operative.
+
 from collections import deque
 
 bullet_price = int(input())
