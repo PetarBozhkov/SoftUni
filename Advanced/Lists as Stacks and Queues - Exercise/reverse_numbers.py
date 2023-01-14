0d5a404072691bc393c67f63b0ef380082fe7d22
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-15 00:40:57 +0200

    Update reverse_numbers.py

--- Advanced/Lists as Stacks and Queues - Exercise/reverse_numbers.py
+++ Advanced/Lists as Stacks and Queues - Exercise/reverse_numbers.py
@@ -1,1 +1,5 @@
+#1. Reverse Numbers
+#Write a program that reads a string with N integers from the console, separated by a single space, and reverses them using a stack. 
+#Print the reversed integers on one line, separated by a single space.
+
 print(*input().split()[::-1], sep=" ")
