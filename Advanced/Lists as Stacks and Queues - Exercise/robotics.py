8fa1bc669483e83bc4aef32ed9a0a16dc7c44a81
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-15 00:46:28 +0200

    Update robotics.py

---
+++ Advanced/Lists as Stacks and Queues - Exercise/robotics.py
@@ -0,0 +1,9 @@
+#7. *Robotics
+#There is a robotics factory. The current project is assembly-line robots.
+#Each robot has a processing time – it is the time in seconds the robot needs to process a product. 
+#When a robot is free, it should take a product for processing and log its name, product, and processing start time.
+#Each robot processes a product coming from the assembly line. 
+#A product is coming from the line each second (so the first product should appear at [start time + 1 second]). 
+#If a product passes the line and there is not a free robot to take it, it should be queued at the end of the line again.
+#The robots are standing in the line in the order of their appearance.
+
