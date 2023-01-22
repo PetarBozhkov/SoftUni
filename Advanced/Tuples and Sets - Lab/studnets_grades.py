92f9f2166b07aa20180ea18474788230f9175255
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-22 16:47:29 +0200

    Update studnets_grades.py

--- Advanced/Tuples and Sets - Lab/studnets_grades.py
+++ Advanced/Tuples and Sets - Lab/studnets_grades.py
@@ -9,7 +9,6 @@ for _ in range(n):
     students_data[student_name].append(float(grade))
 
 for student, grades in students_data.items():
-    print(grades)
     convert_grades_to_string = ' '.join(map(lambda grade: f'{grade:.2f}', grades))
     average_grade = sum(grades) / len(grades)
     print(f'{student} -> {convert_grades_to_string} (avg: {average_grade:.2f})')
