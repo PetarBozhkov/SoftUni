1937057c69cf98c9ca0b4d48ba1db9b507b2bbd3
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-22 16:48:36 +0200

    Update parking_lot.py

--- Advanced/Tuples and Sets - Lab/parking_lot.py
+++ Advanced/Tuples and Sets - Lab/parking_lot.py
@@ -4,6 +4,7 @@ def print_func(data):
             print(car_number)
     else:
         print('Parking Lot is Empty')
+        
 
 n = int(input())
 plate_number_records = [input() for _ in range(n)]
@@ -13,11 +14,8 @@ COMMAND_OUT = 'OUT'
 
 for record in plate_number_records:
     command, plate_number = record.split(', ')
-
     if command == COMMAND_IN:
         parking_lot_data.add(plate_number)
-
     elif command == COMMAND_OUT:
         parking_lot_data.remove(plate_number)
-
 print_func(parking_lot_data)
