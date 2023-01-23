47ca4795a06be25a66df21ffaf63370c79a1a65f
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-24 00:44:10 +0200

    Update count_symobls.py

--- Advanced/Tuples and Sets - Exercise/count_symobls.py
+++ Advanced/Tuples and Sets - Exercise/count_symobls.py
@@ -3,7 +3,6 @@ occurrences = {}
 for letter in input():
     if letter not in occurrences:
         occurrences[letter] = 0
-
     occurrences[letter] += 1
 
 for letter, times in sorted(occurrences.items()):
