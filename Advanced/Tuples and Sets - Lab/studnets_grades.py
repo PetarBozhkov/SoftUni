dd7087fcb3b73fd28ad03f0f80f9f9b2c9a1522b
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-23 20:33:21 +0200

    Update studnets_grades.py

--- Advanced/Tuples and Sets - Lab/studnets_grades.py
+++ Advanced/Tuples and Sets - Lab/studnets_grades.py
@@ -1,7 +1,5 @@
 #2. Students' Grades
-
 #Write a program that reads students' names and their grades and adds them to the student record.
-
 #On the first line, you will receive the number of students – N. 
 #On the following N lines, you will be receiving a student's name and grade. 
 #For each student print all his/her grades and finally his/her average grade, formatted to the second decimal point in the format: "{student's name} -> {grade1} {grade2} ... {gradeN} (avg: {average_grade})".
