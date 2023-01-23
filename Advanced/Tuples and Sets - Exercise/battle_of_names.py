865f30eda2bce39a938b9bc265865913e9f44be8
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-24 00:44:21 +0200

    Update battle_of_names.py

--- Advanced/Tuples and Sets - Exercise/battle_of_names.py
+++ Advanced/Tuples and Sets - Exercise/battle_of_names.py
@@ -3,7 +3,6 @@ even_set = set()
 
 for row in range(1, int(input()) + 1):
     ascii_sum_of_name = sum(ord(l) for l in input()) // row
-
     if ascii_sum_of_name % 2 == 0:
         even_set.add(ascii_sum_of_name)
     else:
