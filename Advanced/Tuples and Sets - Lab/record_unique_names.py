56c5838d7bccb2a5d386010b03ffe1479c0139a7
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-23 20:33:54 +0200

    Update record_unique_names.py

--- Advanced/Tuples and Sets - Lab/record_unique_names.py
+++ Advanced/Tuples and Sets - Lab/record_unique_names.py
@@ -1,7 +1,5 @@
 #3. Record Unique Names
-
 #Write a program, which will take a list of names and print only the unique names in the list.
-
 #The order in which we print the result does not matter
 
 n = int(input())
