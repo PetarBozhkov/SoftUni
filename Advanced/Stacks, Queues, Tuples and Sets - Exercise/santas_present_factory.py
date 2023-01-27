38fb353f76a437f7a69d424dd63ff6acb012c6c7
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-27 17:27:09 +0200

    Update santas_present_factory.py

--- Advanced/Stacks, Queues, Tuples and Sets - Exercise/santas_present_factory.py
+++ Advanced/Stacks, Queues, Tuples and Sets - Exercise/santas_present_factory.py
@@ -1,39 +1,23 @@
 #5. Santa's Present Factory
-
 #This year Santa has decided to share his secret with you. Get ready to learn how his elves craft all the presents.
-
 #First, you will receive a sequence of integers representing the number of materials for crafting toys in one box. 
 #After that, you will be given another sequence of integers – their magic level.
-
 #Your task is to mix materials with magic so you can craft presents, listed in the table below with the exact magic level:
-
 #Present Magic needed
-
 #Doll 150
-
 #Wooden train 250
-
 #Teddy bear 300
-
 #Bicycle 400
-
 #You should take the last box with materials and the first magic level value to craft a toy. 
 #Their multiplication calculates the total magic level. 
 #If the result equals one of the levels described in the table above, you craft the present and remove both materials and magic value. 
 #Otherwise:
-
 #· If the product of the operation is a negative number, you should sum the values together, remove them both from their positions, and add the result to the materials.
-
 #· If the product doesn't equal one of the magic levels in the table and is a positive number, remove only the magic value and increase the material value by 15.
-
 #· If the magic or material (or both) equals 0, remove it (or both) and continue crafting the presents.
-
 #Stop crafting presents when you run out of boxes of materials or magic level values.
-
 #Your task is considered done if you manage to craft either one of the pairs:
-
 #· a doll and a train
-
 #· a teddy bear and a bicycle
 
 from collections import deque
