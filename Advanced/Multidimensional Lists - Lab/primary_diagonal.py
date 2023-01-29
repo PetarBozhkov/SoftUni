b63fb1a3922fafa418aabf5d1b7ab5d55f199263
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-29 13:42:21 +0200

    Update primary_diagonal.py

--- Advanced/Multidimensional Lists - Lab/primary_diagonal.py
+++ Advanced/Multidimensional Lists - Lab/primary_diagonal.py
@@ -1,11 +1,9 @@
 def read_matrix_func():
     number_of_rows = int(input())
     current_matrix = []
-
     for row in range(number_of_rows):
         row_data = list(map(int, input().split(' ')))
         current_matrix.append(row_data)
-
     return current_matrix
 
 
@@ -30,7 +28,6 @@ def get_sum_of_left_half(matrix):
     for row in range(matrix_size):
         for column in range(row + 1):
             sum_of_elements += matrix[row][column]
-
     return sum_of_elements
 
 
@@ -40,8 +37,8 @@ def get_sum_of_right_half(matrix):
     for row in range(matrix_size):
         for column in range(row, matrix_size):
             sum_of_elements += matrix[row][column]
-
     return sum_of_elements
+
 
 print(get_sum_of_primary_diagonal(matrix))
 # print(get_sum_of_secondary_diagonal(matrix))
