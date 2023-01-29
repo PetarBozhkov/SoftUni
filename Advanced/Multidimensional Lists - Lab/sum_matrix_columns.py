842e6dbe79932a02de2f4f7e6fb88dfb5694dba0
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-29 13:43:47 +0200

    Update sum_matrix_columns.py

--- Advanced/Multidimensional Lists - Lab/sum_matrix_columns.py
+++ Advanced/Multidimensional Lists - Lab/sum_matrix_columns.py
@@ -1,11 +1,9 @@
 def read_matrix_func():
     number_of_rows, number_of_columns = map(int, input().split(', '))
     current_matrix = []
-
     for row in range(number_of_rows):
         row_data = list(map(int, input().split(' ')))
         current_matrix.append(row_data)
-
     return current_matrix
 
 
@@ -14,14 +12,11 @@ def get_sum_of_columns():
     rows = len(matrix)
     cols = len(matrix[0])
     sum_of_columns = []
-
     for i in range(cols):
         col_sum = 0
         for j in range(rows):
             col_sum += matrix[j][i]
-
         sum_of_columns.append(col_sum)
-
     return sum_of_columns
 
 
