5151ade00d443259f868ca92e6fdf42ab2e76607
Petar Bozhkov <pbozhkov3@gmail.com>
2023-01-30 21:18:31 +0200

    Update sum_matrix_elements.py

--- Advanced/Multidimensional Lists - Lab/sum_matrix_elements.py
+++ Advanced/Multidimensional Lists - Lab/sum_matrix_elements.py
@@ -1,3 +1,14 @@
+#1. Sum Matrix Elements
+
+#Write a program that reads a matrix from the console and prints:
+
+#· The sum of all numbers in the matrix
+
+#· The matrix itself
+
+#On the first line, you will receive the matrix sizes in the format "{rows}, {columns}". 
+#On the next rows, you will get elements for each column separated by a comma and a space ", ".
+
 def read_matrix_func(test=False):
     if test:
         return [
