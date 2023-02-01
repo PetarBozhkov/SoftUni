b7f332966edc24b0c3d8bae7661ebb490e8ad87c
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-01 14:06:02 +0200

    Update diagonals.py

--- Advanced/Multidimensional Lists - Exercise 1/diagonals.py
+++ Advanced/Multidimensional Lists - Exercise 1/diagonals.py
@@ -1,3 +1,9 @@
+#1. Diagonals
+#Using a nested list comprehension, write a program that reads rows of a square matrix and its elements, separated by a comma and a space ", ". 
+#You should find the matrix's diagonals, prints them and their sum in the format:
+#"Primary diagonal: {element1}, {element2}, … {elementN}. Sum: {sum_of_primary}
+#Secondary diagonal: {element1}, {element2}, … {elementN}. Sum: {sum_of_secondary}".
+
 n = int(input())
 
 matrix = [[int(x) for x in input().split(", ")] for _ in range(n)]
