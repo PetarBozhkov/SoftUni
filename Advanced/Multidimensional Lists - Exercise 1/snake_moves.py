33b8afd2f7201ec7d6d1426893a4f309a4d78eb5
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-01 14:10:56 +0200

    Update snake_moves.py

--- Advanced/Multidimensional Lists - Exercise 1/snake_moves.py
+++ Advanced/Multidimensional Lists - Exercise 1/snake_moves.py
@@ -1,3 +1,10 @@
+#7. Snake Moves
+#You are tasked to visualize a snake's zigzag path in a rectangular matrix with a size N x M.
+#A string represents the snake. It starts moving from the top-left corner to the right. 
+#When the snake reaches the end of the row, it slithers its way down to the next row and turns left. The moves are repeated to the very end.
+#The first cell is filled with the first symbol of the snake. The second cell is filled with the second symbol, etc. 
+#The snake's path is as long as it takes to fill the matrix completely - if you reach the end of the string representing the snake, start again at the first symbol. In the end, you should print the snake's path
+
 from collections import deque
 
 rows, cols = [int(x) for x in input().split()]
