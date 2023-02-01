63fc40146bf8e342b52887c6ec992db13a874b79
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-01 14:06:52 +0200

    Update diagonal_difference.py

--- Advanced/Multidimensional Lists - Exercise 1/diagonal_difference.py
+++ Advanced/Multidimensional Lists - Exercise 1/diagonal_difference.py
@@ -1,3 +1,9 @@
+#2. Diagonal Difference
+#Write a program that finds the difference between the sums of the square matrix diagonals (absolute value).
+#On the first line, you will receive an integer N - the size of a square matrix. 
+#The following N lines hold the values for each column - N numbers separated by a single space. 
+#Print the absolute difference between the primary and the secondary diagonal sums.
+
 num = int(input())
 matrix = [[int(n) for n in input().split()] for row in range(num)]
 
