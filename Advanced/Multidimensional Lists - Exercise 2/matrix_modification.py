cd108a1d91a41a6b04927978cc4ef5060682a88f
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-02 20:26:57 +0200

    Update matrix_modification.py

--- Advanced/Multidimensional Lists - Exercise 2/matrix_modification.py
+++ Advanced/Multidimensional Lists - Exercise 2/matrix_modification.py
@@ -1,21 +1,19 @@
-size = int(input())  # прочитаме редовете
+size = int(input()) 
 matrix = [[int(n) for n in input().split()] for _ in range(size)]
-# създаваме матрица от числа, за всеки ред преобразуваме числата от текст в цели числа
 
-command = input().split()  # прочитаме първата команда
+command = input().split() 
 
-while command[0] != 'END':  # проверяваме дали комадата е END, дъно на нашия цикъл
+while command[0] != 'END': 
     type_command, row, col, num = command[0], int(command[1]), int(command[2]), int(command[3])
-    # прочитаме командата, реда, колоната и числото
+    
 
-    if row < 0 or row >= size or col < 0 or col >= size:  # проверяваме дали координатите са валидни
-        print('Invalid coordinates')  # принтираме съобщението invalid coordinates
-    elif type_command == 'Add':  # проверяваме дали командата е Add
-        matrix[row][col] += num  # добавяме числото към числото на позицията
-    elif type_command == 'Subtract':  # проверяваме дали командата е Subtract
-        matrix[row][col] -= num  # намаляме числото на позицията с текущото число
+    if row < 0 or row >= size or col < 0 or col >= size:  
+        print('Invalid coordinates') 
+    elif type_command == 'Add':  
+        matrix[row][col] += num 
+    elif type_command == 'Subtract':  
+        matrix[row][col] -= num 
 
-    command = input().split()  # четем нова команда
+    command = input().split()  
 
 [print(*row) for row in matrix]
-# за всеки под-лист в матрицата, ънпакваме реда и го принтираме
