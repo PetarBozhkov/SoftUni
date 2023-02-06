5a99bc7d3341fc8169241ca643af0f7c999de1d0
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-06 22:41:22 +0200

    Update knight_game.py

--- Advanced/Multidimensional Lists - Exercise 2/knight_game.py
+++ Advanced/Multidimensional Lists - Exercise 2/knight_game.py
@@ -1,3 +1,14 @@
+#3. Knight Game
+#Chess is the oldest game, but it is still popular these days. It will be used only one chess piece for this task - the Knight.
+#A chess knight has 8 possible moves it can make, as illustrated. 
+#It can move to the nearest square but not on the same row, column, or diagonal. 
+#(e.g., it can move two squares horizontally, then one square vertically, or it can move one square horizontally then two squares vertically - i.e., in an "L" pattern.)
+#The knight game is played on a board with dimensions N x N.
+#You will receive a board with "K" for knights and "0" for empty cells. 
+#Your task is to remove knights until no knights that can attack one another with one move are left.
+#Always remove the knight who can attack the greatest number of knights. 
+#If there are two or more knights with the same number of attacks, remove the top-left one
+
 size = int(input())  
 matrix = [list(input()) for _ in range(size)]  
 
