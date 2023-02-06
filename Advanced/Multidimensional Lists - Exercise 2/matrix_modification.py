dc2ef172c8d510ecf9d594a225a1d2172bc672d0
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-06 22:39:49 +0200

    Update matrix_modification.py

--- Advanced/Multidimensional Lists - Exercise 2/matrix_modification.py
+++ Advanced/Multidimensional Lists - Exercise 2/matrix_modification.py
@@ -1,3 +1,11 @@
+#2. Matrix Modification
+#Write a program that reads a matrix from the console and changes its values. On the first line, you will get the matrix's rows - N. 
+#You will get elements for each column on the following N lines, separated with a single space. You will be receiving commands in the following format:
+#· "Add {row} {col} {value}" – Increase the number at the given coordinates with the value.
+#· "Subtract {row} {col} {value}" – Decrease the number at the given coordinates by the value.
+#If the coordinate is invalid, you should print "Invalid coordinates". A coordinate is valid if both of the given indexes are in range [0; len() – 1].
+#When you receive "END", you should print the matrix and stop the program
+
 size = int(input()) 
 matrix = [[int(n) for n in input().split()] for _ in range(size)]
 
