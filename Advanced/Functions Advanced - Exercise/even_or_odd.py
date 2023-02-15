85eb42321c5301505ffdb4e54dd1fbcc30de16c0
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-15 17:39:27 +0200

    Update even_or_odd.py

--- Advanced/Functions Advanced - Exercise/even_or_odd.py
+++ Advanced/Functions Advanced - Exercise/even_or_odd.py
@@ -1,3 +1,7 @@
+#3. Even or Odd
+#Create a function called even_odd() that can receive a different quantity of numbers and a command at the end. The command can be "even" or "odd". 
+#Filter the numbers depending on the command and return them in a list. Submit only the function in the judge system.
+
 def even_odd(*args):
     command = args[-1]
 
