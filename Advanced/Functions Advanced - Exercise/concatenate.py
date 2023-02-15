383e07e37b4923024f7a9d0c47370041edb6a9f7
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-15 17:40:54 +0200

    Update concatenate.py

--- Advanced/Functions Advanced - Exercise/concatenate.py
+++ Advanced/Functions Advanced - Exercise/concatenate.py
@@ -1,3 +1,12 @@
+#5. Concatenate
+#Write a concatenate() function that receives some strings as arguments and some named arguments 
+#(the key will be a string, and the value will be another string).
+#First, you should concatenate all arguments successively.
+#Next, take each key successively, and if it is present in the resulted string, change all matching parts with the key's value. 
+#In the end, return the final string.
+
+See the examples for more clarification.
+
 def concatenate(*args, **kwargs):
     text = ''.join(args)
 
