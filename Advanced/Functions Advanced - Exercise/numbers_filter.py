a326b3564f496b9d2994a65e2e320d7142cc8aca
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-15 17:40:11 +0200

    Update numbers_filter.py

--- Advanced/Functions Advanced - Exercise/numbers_filter.py
+++ Advanced/Functions Advanced - Exercise/numbers_filter.py
@@ -1,3 +1,11 @@
+#4. Numbers Filter
+#Create a function called even_odd_filter() that can receive a different number of named arguments. 
+#The keys will be "even" and/or "odd", and the values will be a list of numbers.
+#When the key is "odd", you should extract only the odd numbers from its list. 
+#When the key is "even", you should extract only the even numbers from its list.
+#The function should return a dictionary sorted by the length of the lists in descending order. 
+#There will be no case of lists with the same length.
+
 def even_odd_filter(**kwargs):
     if "odd" in kwargs:
         kwargs["odd"] = [n for n in kwargs["odd"] if int(n) % 2 == 1]
