c63050d0a7124946721bd73d032364d4c7532b97
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-15 17:44:29 +0200

    Update age_assignment.py

--- Advanced/Functions Advanced - Exercise/age_assignment.py
+++ Advanced/Functions Advanced - Exercise/age_assignment.py
@@ -1,3 +1,9 @@
+#8. Age Assignment
+#Create a function called age_assignment() that receives a different number of names and a different number of key-value pairs. 
+#The key will be a single letter (the first letter of each name) and the value - a number (age). 
+#Find its first letter in the key-value pairs for each name and assign the age to the person's name.
+#Then, sort the names in ascending order (alphabetically) and return the information for each person on a new line in the format: "{name} is {age} years old."
+
 def age_assignment(*names, **data):
     result = []
 
