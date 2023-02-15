90174975a37d8cf071485a8d55b2391bc204359d
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-15 17:39:00 +0200

    Update keyword_arguments_length.py

--- Advanced/Functions Advanced - Exercise/keyword_arguments_length.py
+++ Advanced/Functions Advanced - Exercise/keyword_arguments_length.py
@@ -1,2 +1,6 @@
+#2. Keyword Arguments Length
+#Create a function called kwargs_length() that can receive some keyword arguments and return their length.
+#Submit only your function in the judge system.
+
 def kwargs_length(**kwargs):
     return len(kwargs.keys())
