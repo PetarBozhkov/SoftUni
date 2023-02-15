6079b24c40ccb1f939df344c617ff33c0ae842b5
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-15 17:42:48 +0200

    Update function_execuitor.py

--- Advanced/Functions Advanced - Exercise/function_execuitor.py
+++ Advanced/Functions Advanced - Exercise/function_execuitor.py
@@ -1,3 +1,10 @@
+#6. Function Executor
+#Create a function called func_executor() that can receive a different number of tuples, each of which will have exactly 2 elements: 
+#the first will be a function, and the second will be a tuple of the arguments that need to be passed to that function. 
+#You should execute each function and return its result in the format:
+#"{function name} - {function result}"
+#For more clarification, see the examples below.
+
 def func_executor(*functions_data):
     return '\n'.join([f"{func.__name__} - {func(*args)}" for func, args in functions_data])
 
