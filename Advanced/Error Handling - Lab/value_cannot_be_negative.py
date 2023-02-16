7c23958a38bdae1b397326d7daf264621b924c15
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-16 18:59:29 +0200

    Update value_cannot_be_negative.py

--- Advanced/Error Handling - Lab/value_cannot_be_negative.py
+++ Advanced/Error Handling - Lab/value_cannot_be_negative.py
@@ -2,7 +2,6 @@ class ValueCannotBeNegative(Exception):
     pass
 
 numbers = [int(input()) for _ in range(5)]
-
 for num in numbers:
     if num < 0:
         raise ValueCannotBeNegative(f'Value cannot be negative number.Convert {num} to {abs(num)}')
