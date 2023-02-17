ae35b7964d6952b58968bb2a424fad6c71aa1b20
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-17 19:02:06 +0200

    Update repeat_text.py

--- Advanced/Error Handling - Lab/repeat_text.py
+++ Advanced/Error Handling - Lab/repeat_text.py
@@ -1,5 +1,4 @@
 #2. Repeat Text
-
 #Write a program that receives a text on the first line and times (to repeat the text) that must be an integer. 
 #If the user passes a non-integer type for the times variable, handle the exception and print a message "Variable times must be an integer".
 
