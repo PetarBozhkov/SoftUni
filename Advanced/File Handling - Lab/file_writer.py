ea1698d5a8cd910b4cffef97965a7b175daa0d37
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-26 23:57:49 +0200

    Update file_writer.py

--- Advanced/File Handling - Lab/file_writer.py
+++ Advanced/File Handling - Lab/file_writer.py
@@ -1,6 +1,5 @@
-3. File Writer
-
-Create a program that creates a file called my_first_file.txt. In that file, write a single line with the content: 'I just created my first file!
+#3. File Writer
+#Create a program that creates a file called my_first_file.txt. In that file, write a single line with the content: 'I just created my first file!
 
 file_path = './my_first_file.txt'
 file = open(file_path, 'w')
