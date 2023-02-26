2d375b3284538dfbc61b64800a700d0a54ff2bcf
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-26 23:55:38 +0200

    Update file_opener.py

--- Advanced/File Handling - Lab/file_opener.py
+++ Advanced/File Handling - Lab/file_opener.py
@@ -1,3 +1,12 @@
+1. File Opener
+
+You are given a file called text.txt with the following text:
+    This is some random line 
+    This is the second line 
+    And this is the third one
+
+Create a program that opens the file. If the file is found, print 'File found'. If the file is not found, print 'File not found'.
+
 file_name = 'text.txt'
 
 try:
