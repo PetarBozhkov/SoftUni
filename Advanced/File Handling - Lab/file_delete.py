0ca0bc38a5ffaf4a075832bd9a22ee21501f70b9
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-26 23:58:40 +0200

    Update file_delete.py

--- Advanced/File Handling - Lab/file_delete.py
+++ Advanced/File Handling - Lab/file_delete.py
@@ -1,6 +1,6 @@
-4. File Delete
-
-Create a program that deletes the file you created in the previous task. If you try to delete the file multiple times, print the message: 'File already deleted!'.
+#4. File Delete
+#Create a program that deletes the file you created in the previous task. 
+#If you try to delete the file multiple times, print the message: 'File already deleted!'.
 
 from os import remove
 from os.path import exists
