f61a3b68c0f5f8be19c4943b9b0884ec639cc510
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-26 23:58:05 +0200

    Update file_reader.py

--- Advanced/File Handling - Lab/file_reader.py
+++ Advanced/File Handling - Lab/file_reader.py
@@ -1,13 +1,11 @@
-2. File Reader
-
-You are given a file called numbers.txt with the following content:
-    1 
-    2 
-    3 
-    4 
-    5
-
-Create a program that reads the numbers from the file. Print on the console the sum of those numbers.
+#2. File Reader
+#You are given a file called numbers.txt with the following content:
+#    1 
+#    2 
+#    3 
+#    4 
+#    5
+#Create a program that reads the numbers from the file. Print on the console the sum of those numbers.
 
 file = open('./numbers.txt')
 result = 0
