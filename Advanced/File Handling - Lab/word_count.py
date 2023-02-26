a6dc0bce1347f4429cbd19bea0717369064eeb69
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-26 23:57:13 +0200

    Update word_count.py

--- Advanced/File Handling - Lab/word_count.py
+++ Advanced/File Handling - Lab/word_count.py
@@ -1,3 +1,9 @@
+5. Word Count
+
+Write a program that reads a list of words from the file words.txt and finds how many times each of the words is contained in another file text.txt. Matching should be case-insensitive.
+
+The results should be written to other text files. Sort the words by frequency in descending order.
+
 import os
 import re
 
