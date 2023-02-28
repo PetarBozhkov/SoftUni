d8140cfbba9e5f30127ed920faac5293f5f2f889
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-28 10:46:47 +0200

    Update even_lines.py

--- Advanced/File Handling - Exercise/even_lines.py
+++ Advanced/File Handling - Exercise/even_lines.py
@@ -1,5 +1,4 @@
 #1. Even Lines
-
 #Write a program that reads a text file and prints on the console its even lines. 
 #Line numbers start from 0. Before you print the result, replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
 
