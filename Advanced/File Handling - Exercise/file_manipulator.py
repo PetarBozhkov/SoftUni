b261d48219014f5034710c6562b4e09167313788
Petar Bozhkov <pbozhkov3@gmail.com>
2023-02-28 10:48:14 +0200

    Update file_manipulator.py

--- Advanced/File Handling - Exercise/file_manipulator.py
+++ Advanced/File Handling - Exercise/file_manipulator.py
@@ -1,3 +1,15 @@
+#3. File Manipulator
+
+#Create a program that will receive commands until the command "End". The commands can be:
+
+#· "Create-{file_name}" - Creates the given file with an empty content. If the file already exists, remove the existing text in it (as if the file is created again)
+
+#· "Add-{file_name}-{content}" - Append the content and a new line after it. If the file does not exist, create it, and add the content
+
+#· "Replace-{file_name}-{old_string}-{new_string}" - Open the file and replace all the occurrences of the old string with the new string.If the file does not exist, print: "An error occurred"
+
+#· "Delete-{file_name}" - Delete the file. If the file does not exist, print: "An error occurred"
+
 import os
 
 while True:
