aa96e33f33d395ef0259d58f0577f7ee6fd74310
Petar Bozhkov <pbozhkov3@gmail.com>
2023-04-25 21:27:49 +0300

    Update class_book.py

--- OOP/First Steps in OOP - Lab/class_book.py
+++ OOP/First Steps in OOP - Lab/class_book.py
@@ -1,13 +1,8 @@
 #3. Class Book
-
 #Create a class called Book. It should have an __init__() method that should receive:
-
 #· name: string
-
 #· author: string
-
 #· pages: int
-
 #Submit only the class in the judge system.
 
 class Book:
