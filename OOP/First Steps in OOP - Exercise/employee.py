4f0d10aed484e5949ff2a971b003c2981abf2997
Petar Bozhkov <pbozhkov3@gmail.com>
2023-04-27 19:24:55 +0300

    Update employee.py

--- OOP/First Steps in OOP - Exercise/employee.py
+++ OOP/First Steps in OOP - Exercise/employee.py
@@ -1,3 +1,10 @@
+#3. Employee
+#Create class Employee. Upon initialization, it should receive id (number), first_name (string), last_name (string) and salary (number). 
+#Create 3 additional instance methods:
+#- get_full_name() - returns "{first_name} {last_name}"
+#- get_annual_salary() - returns the total salary for 12 months
+#- raise_salary(amount) - increases the salary by the given amount and returns the new salary
+
 class Employee:
     def __init__(self, employee_id, first_name, last_name, salary):
         self.id = employee_id
