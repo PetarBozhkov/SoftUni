2f20686993e6d5ed9a7a72f3ab759dcc899e4886
Petar Bozhkov <pbozhkov3@gmail.com>
2023-04-30 23:36:10 +0300

    Update vehicle.py

--- OOP/Classes and Objects - Lab/vehicle.py
+++ OOP/Classes and Objects - Lab/vehicle.py
@@ -1,5 +1,4 @@
 #1. Vehicle
-
 #Create a class called Vehicle. Upon initialization, it should receive max_speed (integer, optional; 150 by default) and mileage (number). 
 #Create an instance variable called gadgets - an empty list by default.
 
