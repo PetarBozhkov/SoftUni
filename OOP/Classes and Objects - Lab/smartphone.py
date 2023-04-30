ddfcf876d0e5322b131372499c51d88403d686f0
Petar Bozhkov <pbozhkov3@gmail.com>
2023-04-30 23:35:43 +0300

    Update smartphone.py

--- OOP/Classes and Objects - Lab/smartphone.py
+++ OOP/Classes and Objects - Lab/smartphone.py
@@ -1,3 +1,20 @@
+#5. Smartphone
+
+#Create a class called Smartphone. Upon initialization, it should receive a memory (number). 
+#It should also have 2 other instance attributes: apps (empty list by default) and is_on (False by default). Create 3 methods:
+
+#- power() - sets is_on on True if the phone is off, otherwise sets it to False
+
+#- install(app, app_memory)
+
+#o If there is enough memory on the phone and it is on, installs the app (add it to apps and decrease the memory of the phone) and returns "Installing {app}"
+
+#o If there is enough memory, but the phone is off, returns "Turn on your phone to install {app}"
+
+#o Otherwise, returns "Not enough memory to install {app}"
+
+#- status() - returns "Total apps: {total_apps_count}. Memory left: {memory_left}
+
 class Smartphone:
 
     def __init__(self, memory):
